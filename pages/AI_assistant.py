e0a3e16602755bd4a496ab445d9b72ffbeb6fd85
hkriaz <HK857@live.mdx.ac.uk>
2025-12-11 19:01:27 +0400

    Update AI_assistant.py

--- pages/AI_assistant.py
+++ pages/AI_assistant.py
@@ -4,7 +4,7 @@ from google import genai
 st.set_page_config(page_title='CST1510 CW2')
 st.title("ASK ME ANYTHING")
 
-client = genai.Client(api_key="AIzaSyA9jO0XEesIeNLdpNVa3OmqOVjPnq-Fm3s")
+client = genai.Client(api_key="")
 query = st.text_input("Enter your query:")
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents=query
