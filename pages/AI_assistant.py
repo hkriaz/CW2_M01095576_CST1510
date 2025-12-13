31597db1ca6f7879d5cf5ed1219999912a97d2f7
hkriaz <HK857@live.mdx.ac.uk>
2025-12-13 17:30:32 +0400

    Update AI_assistant.py

--- pages/AI_assistant.py
+++ pages/AI_assistant.py
@@ -1,12 +1,23 @@
 import streamlit as st
 from google import genai
 
-st.set_page_config(page_title='CST1510 CW2')
+# Page configuration
+st.set_page_config(page_title='CST1510 CW2', page_icon="img/mdi.jpg")
 st.title("ASK ME ANYTHING")
 
-client = genai.Client(api_key="")
+# Retrieve the private key from Streamlit secrets
+api_key = st.secrets["GOOGLE_API_KEY"]
+
+# Initialise the Gemini client with the hidden key
+client = genai.Client(api_key=api_key)
+
+# User query
 query = st.text_input("Enter your query:")
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=query
-)
-st.write(response.text)
+
+# Generate the response (only if a query was entered)
+if query:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=query
+    )
+    st.write(response.text)
